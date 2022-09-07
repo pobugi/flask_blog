7760fc6a4567226c4a29c96351d308c8a34d9794
Evgeny Stepanchenko <pobugi@gmail.com>
2022-09-07 11:26:37 +0300

    excessive logic removed

--- src/__init__.py
+++ src/__init__.py
@@ -13,13 +13,11 @@ def create_app():
     app.config.from_object(app_settings)
 
     db.init_app(app)
-    from src.api.file.views import file_api
     from src.api.post.views import post_api
     from src.api.user.views import user_api
     from src.api.post_comment.views import post_comment_api
     from src.api.post_like.views import post_like_api
 
-    app.register_blueprint(file_api)
     app.register_blueprint(post_api)
     app.register_blueprint(user_api)
     app.register_blueprint(post_comment_api)
